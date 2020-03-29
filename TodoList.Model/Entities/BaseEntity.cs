c46396d94e60e633e570113090ed83f1ae7f6245
Hacer <Hacer@DESKTOP-2JMK2LJ>
2020-03-29 18:56:28 +0300

    Add project files.

---
+++ TodoList.Model/Entities/BaseEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Model.Entities
+{
+    public class BaseEntity
+    {
+        [Column("created_date")]
+        public long CreatedDate { get; set; }
+
+        [Column("updated_date")]
+        public long UpdatedDate { get; set; }
+
+        [Column("deleted_date")]
+        public long? DeletedDate { get; set; }
+
+        [Column("owner_by")]
+        [StringLength(80)]
+        public string OwnerBy { get; set; }
+
+        [Column("modifier_by")]
+        [StringLength(80)]
+        public string ModifierBy { get; set; }
+
+        [Column("status")]
+        public byte? Status { get; set; }
+    }
+}
---
+++ TodoList.Model/Entities/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Model.Entities
+{
+    public class User:BaseEntity
+    {
+        [Column("id")]
+        public long Id { get; set; }
+
+        [Column("user_name")]
+        [Required, StringLength(80)]
+        public string UserName { get; set; }
+
+        [Column("full_name")]
+        [Required, StringLength(80)]
+        public string FullName { get; set; }
+
+        [Column("password")]
+        [Required, StringLength(80)]
+        public string Password { get; set; }
+    }
+}
